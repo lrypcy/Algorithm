781a8c4c2f7272f180e1444e813de4910f49a93d
whuer_pcy <p_c_yuan@whu.edu.cn>
2019-08-18 18:01:39 +0800

    Update segmenttree.h

--- tree/segmenttree.h
+++ tree/segmenttree.h
@@ -2,4 +2,5 @@
 Some references:
   1. https://blog.csdn.net/zearot/article/details/48299459
   2. https://blog.csdn.net/huangzihaoal/article/details/81813454
+  3. https://www.cnblogs.com/TheRoadToTheGold/p/6254255.html
 */
